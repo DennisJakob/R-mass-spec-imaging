37b82f7dce37243c2ec58d079cd48ea00283f0a4
Dennis Jakob <dennisjakob@gmx.de>
2019-07-22 14:15:52 +0200

    loop implemented
    -> output generated zeros

--- intensity_matrix_loop.R
+++ intensity_matrix_loop.R
@@ -3,7 +3,7 @@ library("tictoc")
 library("Cardinal")
 
 # file name
-file <- "tmp/20190523_MS37_MetaboliteMix_Spots_60_900_SDHB_pos_A25_75um_88x70" 
+file <- "tmp/20190523_MS37_MetaboliteMix_Spots_60_900_SDHB_pos_A25_75um_88x70" #args[1]
 
 #-------------------------------------------------------------
 
@@ -11,32 +11,54 @@ file <- "tmp/20190523_MS37_MetaboliteMix_Spots_60_900_SDHB_pos_A25_75um_88x70"
 msi <- readImzML(file,
                  attach.only = FALSE,
                  as="MSImagingExperiment",
-                 resolution = 0.001,
-                 units = "mz")
+                 resolution = 0.001, #args [2]
+                 units = "mz") #args [3]
 
 # define fragmentation
 pixel       <- as.character(seq(1, ncol(msi),1))
 mz          <- as.vector(mz(msi))
-range       <- 50000
+range       <- 50000 #args [4]
 
-# create fragmentation matrix
-mz_range <- matrix(1:(range*floor(length(mz)/range)),
-                   nrow = range,
-                   ncol = floor(length(mz)/range))
-last_mz <- seq(from = floor(length(mz)/range)*range+1,
-               to = length(mz),
-               by = 1)
-
-# create mz_range matrix
-mz_values <- matrix(ncol = ncol(mz_range),
-                    nrow = range)
-for (i in 1:ncol(mz_range)) {
-  mz_values[,i] <- mz[mz_range[,i]]
+# create range list
+range_list <- list()
+range_list[[1]] <- 1:range
+for (i in 2:floor(length(mz)/range)) {
+  range_list[[i]] <- range_list[[i-1]]+range
 }
-last_mz_values <- mz[last_mz]
+range_list[[length(range_list)+1]] <- (floor(length(mz)/range)*range+1):length(mz)
+
+# create mz list
+mz_list <- list()
+for (i in 1:length(range_list)) {
+  mz_list[[i]] <- mz[range_list[[i]]]
+}
 
 # create minor matrices
-i_threshold <- 100
+i_threshold <- 100 #args [5]
+
+
+matrix_list <- list()
+
+for (i in 1:length(mz_list)) {
+  matrix_list[[i]]            <- spectra(msi)[mz_list[[i]],]
+  colnames(matrix_list[[i]])  <- pixel
+  rownames(matrix_list[[i]])  <- mz_list[[i]]
+  matrix_list[[i]]            <- matrix_list[[i]][rowSums(matrix_list[[i]] > i_threshold) >= 1, ] # strip matrix by deleting peaks under threshold
+  matrix_list[[i]]            <- t(matrix_list[[i]])
+  mz_list[[i]]                <- as.numeric(colnames(matrix_list[[i]]))
+  matrix_list[[i]]            <- rbind(mz_list[[i]], matrix_list[[i]])
+}
+
+for (i in 1:length(mz_list)) {
+  matrix_list[[i]]             <- spectra(msi)[mz_list[[i]],]
+  matrix_list[[i]]             <- t(matrix_list[[i]])
+  colnames(matrix_list[[i]])   <- mz_list[[i]]
+  matrix_list[[i]]             <- matrix_list[[i]][, colSums(matrix_list[[i]] > i_threshold) >= 1]
+  mz_list[[i]]                 <- as.numeric(colnames(matrix_list[[i]]))
+  rownames(matrix_list[[i]])   <- pixel
+  matrix_list[[i]]             <- rbind(mz_list[[i]], matrix_list[[i]])
+}
+
 
 tictoc::tic("matrix1")
 matrix1             <- spectra(msi)[range1,]
