3982c408e0e6a65910a56c8efe1918fc50113d49
Dennis Jakob <dennisjakob@gmx.de>
2019-08-07 10:34:48 +0200

    updated with prefixes

--- crop_imzML.R
+++ crop_imzML.R
@@ -1,16 +1,26 @@
 # edit imzML 
+library(Cardinal)
 
-# crop ms image
-msidata <- readImzML("datasets/20190124_MS37_MetaboliteMix_Spots_60_900_HCCA_pos_A28_100um_75x89",
+ImzMLfile <- "tmp/20190124_MS37_MetaboliteMix_Spots_60_900_HCCA_pos_A28_100um_75x89"
+binwidth <- 0.001
+pixelrange <- 1692:4361 # to keep
+outfile <- "tmp/20190510_MetaboliteMix_HCCAp_60_900_A28_100um_25x89"
+
+
+# import old dataset
+msidata <- readImzML(ImzMLfile,
                      attach.only = FALSE,
                      as="MSImagingExperiment",
-                     resolution = 0.0005, # binning width
+                     resolution = binwidth, # binning width
                      units = "mz")
-msidata2 <- msidata[,1692:4361] #30x74
+# crop dataset
+msidata2 <- msidata[,pixelrange]
+# write new dataset
 writeImzML(msidata2,
-           "20190510_MetaboliteMix_HCCAp_60_900_A28_100um_25x89")
-msidata2 <- readImzML("datasets/20190510_MetaboliteMix_HCCAp_60_900_A28_100um_25x89",
+           outfile)
+# import new dataset
+msidata2 <- readImzML(outfile,
                       attach.only = FALSE,
-                      as="MSImageSet",
-                      resolution = 0.0005, # binning width
+                      as="MSImagingExperiment",
+                      resolution = binwidth, # binning width
                       units = "mz")
